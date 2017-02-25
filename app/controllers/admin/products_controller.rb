d07177d20ab8bbecd004cf78fbe4d959fb6774aa
Ruslan Rozhkov <bladteam@gmail.com>
2017-02-25 13:37:10 +0200

    Added edit function to product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all.limit(10)
   end
@@ -12,16 +14,42 @@ class Admin::ProductsController < ApplicationController
     @product = Product.create(product_params)
     respond_to do |format|
       if @product.save
-        format.html { redirect_to admin_products_path, notice: 'Товар добавлен успешно' }
+        format.html { redirect_to admin_products_path, notice: 'Товар успешно добавлен' }
       else
         format.html { render :new }
       end
     end
   end
 
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to admin_products_path, notice: 'Товар успешно обновлен' }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @product.destroy
+        format.html { redirect_to admin_products_path, notice: 'Товар удален' }
+      else
+        format.html { redirect_to admin_products_path, notice: 'Невозможно удалить товар' }
+      end
+    end
+  end
+
   private
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
-    params.require(:product).permit(:category_id, :title, :description)
+    params.require(:product).permit(:id, :category_id, :title, :description)
   end
 end
