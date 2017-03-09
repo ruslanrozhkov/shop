85cbfef135f36fc642133968316e1a9dadafdf6b
Ruslan Rozhkov <bladteam@gmail.com>
2017-03-09 15:29:55 +0200

    Added categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,13 @@ class CategoriesController < ApplicationController
   before_action :set_category
 
   def show
+    @categories = Category.all
     @products = @category.products.all
+    size = ParameterName.find_by_name('size')
+    color = ParameterName.find_by_name('color')
+    @sizes = size.parameter_values
+    @colors = color.parameter_values
+
   end
 
   private
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,8 @@ class ProductsController < ApplicationController
   before_action :set_product
 
   def show
-    size = ParameterName.where(name: 'size')
-    color = ParameterName.where(name: 'color')
+    size = ParameterName.find_by_name('size')
+    color = ParameterName.find_by_name('color')
     @product
     @sizes = @product.parameter_values.where(parameter_name_id: size)
     @colors = @product.parameter_values.where(parameter_name_id: color)
