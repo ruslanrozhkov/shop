b7c445c9767f996ed78097862ee878a0607d2b42
Ruslan Rozhkov <bladteam@gmail.com>
2017-03-09 23:25:51 +0200

    Added product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,14 @@ class ProductsController < ApplicationController
   before_action :set_product
 
   def show
+    @product
+    @categories = Category.all
     size = ParameterName.find_by_name('size')
     color = ParameterName.find_by_name('color')
-    @product
-    @sizes = @product.parameter_values.where(parameter_name_id: size)
-    @colors = @product.parameter_values.where(parameter_name_id: color)
+    @sizes = size.parameter_values
+    @colors = color.parameter_values
+    @product_sizes = @product.parameter_values.where(parameter_name_id: size)
+    @product_colors = @product.parameter_values.where(parameter_name_id: color)
   end
 
   private
